590ade0cde87d6a06e9d742f6004267a0bfa8836
phylynxbelt <thanaporn.jira@odds.team>
2021-08-08 17:00:47 +0700

    :zap: fix token encrypt

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,7 @@
 const functions = require('firebase-functions');
-const region = 'asia-southeast-1';
+const region = 'asia-southeast1';
 
-const axios = require('axios');
-
-const LINE_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN
+const LINE_ACCESS_TOKEN = functions.config().linebot.access_token;
 
 const memeBot = require('./memeBot');
 const utils = require('./utils');
@@ -16,11 +14,27 @@ exports.lineBot = functions.region(region).https.onRequest((request, response) =
 
         if (messageType == 'text') {
             const textMessage = request.body.events[0].message.text;
-            utils.reply(
-                utils.lineBotHeader(LINE_ACCESS_TOKEN),
-                request.body.events[0].replyToken,
-                { type: 'text', text: textMessage }
-            );
+            
+            let result;
+            if (textMessage.includes('สวัสดี')) {
+                result = {
+                    type: 'text',
+                    text: 'สวัสดี'
+                }
+            } else {
+                result = {
+                    type: 'text',
+                    text: memeBot.getMemeWording()
+                };    
+            }
+
+            if (result !== '') {
+                utils.reply(
+                    utils.lineBotHeader(LINE_ACCESS_TOKEN),
+                    request.body.events[0].replyToken,
+                    result
+                );
+            }
         }
     }
     return response.status(200).send(request.method)
